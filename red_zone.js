3e8d4bfab0259c188df2d72ba247334962317d15
SuhaanArora <71770249+SuhaanArora@users.noreply.github.com>
2020-10-24 16:56:38 +0530

    Add files via upload

---
+++ red_zone.js
@@ -0,0 +1,19 @@
+class redZone {
+
+constructor(x,y,width,height){
+    var options={
+    isStatic = true 
+    }
+
+    this.body = Bodies.rectangle (x,y,width,height,options)
+   World.add(world,this.body)
+}
+  display(){
+    var pos = this.body.position;
+    rectMode(CENTER)
+    fill ("red")
+    rect (pos.x,pos.y,this.width,this.height) 
+  }
+ 
+
+}
